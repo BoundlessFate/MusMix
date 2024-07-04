7ea706c216d0f5ce55d690bf04a81dca9fad4018
BoundlessFate <villac3@rpi.edu>
2024-07-04 10:26:13 -0400

    finished songdatabase script

--- webapp/src/app/scripts/databaseCreator.py
+++ webapp/src/app/scripts/databaseCreator.py
@@ -5,7 +5,7 @@ import time
 
 fileIn = "allgenres.txt"
 fileOut = "songdatabase.json"
-songsPerGenre = 250
+songsPerGenre = 500
 
 clientId = "89ed1b35def340909aa9b514ed93291e"
 clientSecret = "1f50f138da1d42cd8adeedbbe904d8cd"
@@ -19,7 +19,7 @@ def writeSongData(fOut, songs, sp, songsLeft):
     featuresFull = sp.audio_features(songIds)
     i = 0
     for song, features in zip(songs['tracks']['items'], featuresFull):
-        fOut.write("\t\t\t\""+song['name']+" - "+song['artists'][0]['name']+"\": {\n")
+        fOut.write("\t\t\t\""+song['name'].replace('"', '\\"')+" - "+song['artists'][0]['name'].replace('"', '\\"')+"\": {\n")
         fOut.write("\t\t\t\t\"title\": \""+song['name'].replace('"', '\\"')+"\",\n")
         fOut.write("\t\t\t\t\"artist\": \""+song['artists'][0]['name'].replace('"', '\\"')+"\",\n")
         fOut.write("\t\t\t\t\"release_date\": \""+song['album']['release_date']+"\",\n")
@@ -32,11 +32,11 @@ def writeSongData(fOut, songs, sp, songsLeft):
         fOut.write("\t\t\t\t\"energy\": \""+str(features['energy'])+"\",\n")
         fOut.write("\t\t\t\t\"speechiness\": \""+str(features['speechiness'])+"\",\n")
         fOut.write("\t\t\t\t\"valence\": \""+str(features['valence'])+"\"\n")
+        i += 1
         if i >= songsLeft:
             fOut.write("\t\t\t}\n")
         else:
             fOut.write("\t\t\t},\n")
-        i += 1
     return
 
 with open(fileIn, 'r') as fIn:
@@ -58,21 +58,36 @@ with open(fileIn, 'r') as fIn:
                 # Or 1 for every third of a second
                 time.sleep(0.333)
                 i = 50
-                if (songsLeft < 50):
+                if (songsLeft < 100):
                     i = songsLeft
-                allSongs = sp.search(q=f"genre:{genre}", type="track", limit=i, offset=page)
+                keysToRemove = []
+                allSongs = dict()
+                if (songsLeft > 50):
+                    songsOne = sp.search(q=f"genre:{genre}", type="track", limit=i, offset=page)
+                    songsTwo = sp.search(q=f"genre:{genre}", type="track", limit=i, offset=page+1)
+                    allSongs = songsOne
+                    allSongs['tracks']['items'] = allSongs['tracks']['items'] + songsTwo['tracks']['items']
+                    page += 2
+                else:
+                    allSongs = sp.search(q=f"genre:{genre}", type="track", limit=i, offset=page)
+                    page += 1
                 for song in allSongs['tracks']['items']:
                     if song['name']+" "+song['artists'][0]['name'] in songList:
                         songsLeft -= 1
-                        allSongs.remove(song)
+                        keysToRemove.append(song)
                         continue
                     else:
                         songList.append(song['name']+" "+song['artists'][0]['name'])
+                for key in keysToRemove:
+                    allSongs['tracks']['items'].remove(key)
                 # Writes the song data for a given song from the current genre
                 writeSongData(fOut, allSongs, sp, songsLeft)
-                songsLeft -= len(allSongs)
-                page += 1
+                songsLeft -= len(allSongs['tracks']['items'])
             line = fIn.readline()
+            if (line == ""):
+                fOut.write("\t\t}\n")
+            else:
+                fOut.write("\t\t},\n")
         fOut.write("\t}\n}")
     fOut.close()
 fIn.close()
