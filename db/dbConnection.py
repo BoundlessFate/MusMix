49ac807ffa9172f7a1cd51fada1c69e52df0ed91
noobzhuz <75611029+noobzhuz@users.noreply.github.com>
2024-07-07 21:26:25 -0400

    fixed context issue

--- db/dbConnection.py
+++ db/dbConnection.py
@@ -1,10 +1,15 @@
-from flask import g
+from flask import current_app, g
 import pymongo
 
-
+# return a client.
 def connect_mongo():
+    """
+    Returns:
+        MongoDB client
+    """
+    app = current_app._get_current_object()
     if 'db' not in g:
         g.db = pymongo.MongoClient("mongodb+srv://MusMixAdmin1:uGpw6A07tp2kbTP7@musmix.bb1xivw.mongodb.net/",
-                                    username = "MusMixAdmin1", password = "uGpw6A07tp2kbTP7").get_database()
+                                    username = "MusMixAdmin1", password = "uGpw6A07tp2kbTP7").get_database("musmixdata")
     return g.db
 
--- db/login.py
+++ db/login.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, request, current_app
 import pymongo
 from sha import sha256
 import dbConnection
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['?'])
+client = None
+collection = None
+db = None
+with app.app_context():
+    db = dbConnection.connect_mongo()
+    #db = client["musmixdata"]
+    collection = db['userdata']
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def user_login():
-    
-    
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        auth = sha256.generate_hash(username + password).hex()
+        result = collection.find_one({'username': username, 'auth': auth})
+        if result == None:
+            print("Incorrect username or password")
+        else:
+            print("Login success")
+
+if __name__ == '__main__':
+    app.run(debug=True)
