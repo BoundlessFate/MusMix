43235431ceb767c49e0533b3d154b1115468cd1a
noobzhuz <75611029+noobzhuz@users.noreply.github.com>
2024-07-11 11:40:34 -0400

    flask work locally

--- db/login.py
+++ db/login.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pymongo
 from sha import sha256
 import dbConnection
 
 app = Flask(__name__)
+CORS(app)
 
 client = None
 collection = None
@@ -17,11 +19,14 @@ with app.app_context():
 def user_login():
     print(1111)
     if request.method == "POST":
+        print(2222222)
         data = request.get_json()
         username = data.get('username')
         password = data.get('password')
+        print(username)
+        print(password)
         auth = sha256.generate_hash(username + password).hex()
-        result = collection.find_one({'username': username, 'auth': auth})
+        result = collection.find_one({'auth': auth})
         if result is None:
             print("Incorrect username or password")
             return jsonify({"message": "Incorrect username or password"}), 404
