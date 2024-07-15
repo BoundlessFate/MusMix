824ab5239af54c0e3c5b6a36d9d16071bcf1979a
BoundlessFate <villac3@rpi.edu>
2024-07-15 15:31:22 -0400

    finished cookies and register page sorta

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { SearchComponent } from './search/search.component';
+import { ProfileComponent } from './profile/profile.component'
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'searchPage', component: SearchComponent}
+    { path: 'search', component: SearchComponent},
+    { path: 'profile', component: ProfileComponent}
 ];
