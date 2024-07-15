d9bc92e2a3ad010a098977802347201fc60fd477
BoundlessFate <villac3@rpi.edu>
2024-07-15 10:42:59 -0400

    commiting to github

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { SearchComponent } from './search/search.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: 'searchPage', component: SearchComponent}
 ];
