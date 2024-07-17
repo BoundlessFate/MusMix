e98db2cb5442bf002f4c2d36453829930c302195
tarafeeney <114501013+tarafeeney@users.noreply.github.com>
2024-07-17 17:11:03 -0400

    PARTIALLY DONE

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SearchComponent } from './search/search.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'searchPage', component: SearchComponent}
+    { path: 'searchPage', component: SearchComponent},
+    { path: 'profile', component: ProfileComponent}
 ];
