4774e948a249a653dccac2e77e253cc610e58810
BoundlessFate <villac3@rpi.edu>
2024-07-18 15:49:12 -0400

    added search functionality

--- webapp/src/app/search/search.component.ts
+++ webapp/src/app/search/search.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-search',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './searchPage.html',
@@ -13,20 +13,51 @@ import { RouterOutlet } from '@angular/router';
 // adding in variables
 export class SearchComponent {
   title = 'Search Page';
-  username = "NEW USER"
+  finalString = "";
   ngOnInit() {
     this.checkForSubmission();
   }
   async checkForSubmission() {
     console.log("hi");
     window.addEventListener('DOMContentLoaded', (event) => {
-      const searchInput = document.getElementById('input') as HTMLInputElement;
+      const songInput = document.getElementById('songInput') as HTMLInputElement;
+      const artistInput = document.getElementById('artistInput') as HTMLInputElement;
   
-      searchInput.addEventListener('keydown', (e: KeyboardEvent) => {
+      songInput.addEventListener('keydown', (e: KeyboardEvent) => {
           if (e.key === 'Enter') {
-              alert('You have submitted a song with query: ' + searchInput.value);
+              console.log("pressed enter")
+              this.search_song()
           }
       });
+      artistInput.addEventListener('keydown', (e: KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            console.log("pressed enter")
+            this.search_song()
+        }
     });
+    });
+  }
+  async search_song() {
+    try {
+      const name = (document.getElementById('songInput') as HTMLInputElement).value;
+      const artist = (document.getElementById('artistInput') as HTMLInputElement).value;
+      console.log(name)
+      console.log(artist)
+      const response = await fetch('http://127.0.0.1:5002/search', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ name, artist })
+        })
+        const data = await response.json();
+      this.finalString = data.message;
+      alert("success");
+    } catch (error) {
+      console.error('Error:', error);
+      alert("error");
+      window.location.reload();
+      this.finalString = "";
+    }
   }
 }
