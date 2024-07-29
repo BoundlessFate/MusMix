e5981da9c6887d5324a5eda3f623a9b2d91775df
BoundlessFate <villac3@rpi.edu>
2024-07-29 11:36:03 -0400

    test 3

--- webapp/start_server.sh
+++ webapp/start_server.sh
@@ -6,10 +6,3 @@ NG_PID=$!
 
 python3 ../db/backend_python.py &
 PYTHON_PID=$!
-
-# Wait for a key to be pressed before exiting
-read -p "Press any key to terminate processes..." -n1 -s
-
-# Terminate processes
-kill $NG_PID
-kill $PYTHON_PID
